ef42c29069e8e4797420babdc1eb4ffe0feb2620
zxlim92 <55634410+zxlim92@users.noreply.github.com>
2023-07-18 12:19:16 -0600

    change bg

--- src/components/Backgroud.js
+++ src/components/Backgroud.js
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 import './Background.css';
 
 export default function Background() {
-  const spline_url = "https://prod.spline.design/nKgCDgXSzmVGsHdq/scene.splinecode"
+  const spline_url = "https://prod.spline.design/Z-pKguv2uhkuchiv/scene.splinecode"
   return (
     <div className="spline-container">
       <div className="gradient-overlay"></div>
